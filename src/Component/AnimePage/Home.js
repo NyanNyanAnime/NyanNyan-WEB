1d1c301c11118463691d4d6b40b1852db8921309
Renn243 <muhraihanresa243@gmail.com>
2024-11-28 22:43:41 +0800

    fix finished anime link

--- src/Component/AnimePage/Home.js
+++ src/Component/AnimePage/Home.js
@@ -283,7 +283,7 @@ const Home = () => {
                     </div>
                     <div className='grid grid-cols-2 sm:grid-cols-4 gap-4 px-4 mb-8'>
                         {finishedData.data.slice(0, 8).map((res) => (
-                            <Link to={`/anime/${res.anime_id}`} key={res.anime_id}>
+                            <Link to={`/anime/details/${res.anime_id}`} key={res.anime_id}>
                                 <div className='w-full bg-white shadow relative overflow-hidden rounded-lg hover:transform duration-300 hover:-translate-y-2'>
                                     <img className='h-32 sm:h-64 w-full rounded-lg object-cover' src={res.image} alt={res.title} />
                                     <h3 className='absolute bottom-0 left-0 px-2 py-2 text-xs sm:text-md font-semibold text-white'>{truncateText(res.title, 20)}</h3>
